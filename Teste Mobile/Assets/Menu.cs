9654add9e72e8c185f07a3c92600c6a12761352c
Cláuvin <Clauvin@users.noreply.github.com>
2020-06-05 03:54:43 -0300

    Passing project to the right place

---
+++ Teste Mobile/Assets/Menu.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Android;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+
+    public Button start;
+    public Button form;
+    public Button help;
+    public Button exit;
+    public Button back;
+    public Button yes;
+    public Button no;
+
+    public GameObject formPanel;
+    public GameObject helpPanel;
+    public GameObject exitPanel;
+
+    void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        formPanel.SetActive(false);
+        helpPanel.SetActive(false);
+        exitPanel.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Function for Start Button, loads Main Scene 
+    public void StartPress()
+    {
+        Debug.Log("ops");
+        SceneManager.LoadScene(1);
+    }
+
+    public void FormPress()
+    {
+        formPanel.SetActive(true);
+
+    }
+
+    // Function for Help Button, enables Help Panel
+    public void HelpPress()
+    {
+        helpPanel.SetActive(true);
+    }
+
+    // Function for Exit Button, exits the application
+    public void ExitPress()
+    {
+        exitPanel.SetActive(true);
+    }
+
+    // Function for Back Button, disables Help Panel
+    public void BackPress()
+    {
+        helpPanel.SetActive(false);
+    }
+
+    public void YesPress()
+    {
+        //System.Diagnostics.Process.GetCurrentProcess().Kill();
+        Application.Quit();
+    }
+    public void NoPress()
+    {
+        Debug.Log("ei não");
+        exitPanel.SetActive(false);
+    }
+}
---
+++ Teste Mobile/Assets/Tests/TestScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestScript
+    {
+        [SetUp]
+        public void ResetScene()
+        {
+
+            
+
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+        [Test]
+        public void BasicTest()
+        {
+
+            bool isActive = false;
+
+            Assert.AreEqual(false, isActive);
+
+        }
+
+        [Test]
+        public void CatchingErrors()
+        {
+
+            GameObject gameObject = new GameObject("test");
+
+            Assert.Throws<MissingComponentException>(
+                () => gameObject.GetComponent<Rigidbody>().velocity = Vector3.one
+            );
+
+        }
+    }
+}
