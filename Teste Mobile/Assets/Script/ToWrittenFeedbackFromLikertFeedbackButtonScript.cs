3a37cd6750cbaa2bd5fa7c8f285b86d3da99fc24
Cláuvin <Clauvin@users.noreply.github.com>
2020-07-27 23:47:37 -0300

    (#4) Unit test for Likert Feedback Panel in progress

---
+++ Teste Mobile/Assets/Script/ToWrittenFeedbackFromLikertFeedbackButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToWrittenFeedbackFromLikertFeedbackButtonScript : ButtonScript
+{
+    public GameObject feedbackPanelManager;
+    FeedbackPanelManagerScript feedbackPanelManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadFeedbackPanelManager();
+    }
+    private void loadFeedbackPanelManager()
+    {
+        feedbackPanelManagerScript = feedbackPanelManager.GetComponent<FeedbackPanelManagerScript>();
+    }
+
+    public override void whenPressed()
+    {
+        if ((feedbackPanelManagerScript == null) && (feedbackPanelManager != null)) loadFeedbackPanelManager();
+        else if (feedbackPanelManager == null)
+        {
+            throw new System.NullReferenceException("BackToMainFromLikertFeedbackButtonScript.feedbackPanelManager is null.");
+        }
+
+        feedbackPanelManagerScript.hideLikertFeedbackPanel();
+        Debug.Log(feedbackPanelManagerScript.showWrittenFeedbackPanel());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Teste Mobile/Assets/Tests/LikertFeedbackPanelTestScript.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    [TestFixture]
+    [Author("Cláuvin", "")]
+    public class LikertFeedbackPanelTestScript
+    {
+        GameObject main_panel_manager;
+        GameObject main_panel;
+        GameObject feedback_panel;
+
+        [OneTimeSetUp]
+        public void WriteStartOfLog()
+        {
+            WriteTestLogScript.WriteString("AboutPanelTestScript tests starting.");
+        }
+
+        [SetUp]
+        public void ResetScene()
+        {
+            TestContext.WriteLine("Setup started");
+            GameObject the_main = (GameObject)GameObject.Instantiate(Resources.Load("Prefab/Prefab Main Menu"));
+            the_main.name = "Prefab Main Menu";
+            TestContext.WriteLine("Setup finished");
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void BackButtonWorks()
+        {
+            Action del = this.BackButtonWorks;
+            string ret = del.Method.Name;
+
+            UnityEngine.Object[] list = Resources.FindObjectsOfTypeAll(typeof(GameObject));
+
+            main_panel_manager = GameObject.Find("Prefab Main Menu");
+            main_panel = main_panel_manager.GetComponentInChildren<MainPanelManagerScript>().mainPanel;
+            feedback_panel = main_panel_manager.GetComponentInChildren<MainPanelManagerScript>().feedbackPanel;
+
+            try
+            {
+                WriteTestLogScript.WriteString("Starting " + ret + " test.");
+                FeedbackButtonScript fb_script = main_panel_manager.GetComponentInChildren<MainPanelManagerScript>().
+                    mainFeedbackButton.GetComponent<FeedbackButtonScript>();
+                fb_script.whenPressed();
+                BackToMainFromLikertFeedbackButtonScript bm_script = feedback_panel.transform.GetChild(1).GetChild(1).
+                    GetComponent<BackToMainFromLikertFeedbackButtonScript>();
+                bm_script.whenPressed();
+                Assert.AreEqual(main_panel.activeSelf, true);
+            }
+            catch (AssertionException ae)
+            {
+                WriteTestLogScript.TestFailed(ret);
+                Assert.Fail();
+                return;
+            }
+
+            WriteTestLogScript.TestPassed(ret);
+        }
+    }
+}
+
