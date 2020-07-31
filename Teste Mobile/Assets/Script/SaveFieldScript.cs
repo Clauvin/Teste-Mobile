7ddf8d5fb2eba815d378691b5944dd094c69be8f
Cláuvin <Clauvin@users.noreply.github.com>
2020-07-31 00:03:47 -0300

    (#4) Created SaveFieldScript

---
+++ Teste Mobile/Assets/Script/SaveFieldScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveFieldScript : MonoBehaviour
+{
+    public string name = "";
+    public string value = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SendDataToSaveManager()
+    {
+        SaveManagerScript.AddData(name, value);
+    }
+}
