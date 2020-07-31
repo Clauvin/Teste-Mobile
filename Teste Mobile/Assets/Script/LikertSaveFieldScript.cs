323fcb0db46a4129c57cfee3c3e513559b518f4e
Cláuvin <Clauvin@users.noreply.github.com>
2020-07-31 00:04:22 -0300

    (#4) Created LikertSaveFieldScript

---
+++ Teste Mobile/Assets/Script/LikertSaveFieldScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LikertSaveFieldScript : SaveFieldScript
+{
+    public void FromLikertToField()
+    {
+        value = GetComponent<Slider>().value.ToString();
+    }
+}
