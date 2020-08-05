959b41e8153baf2a8a040e894fbba7b12ba09a17
Cláuvin <Clauvin@users.noreply.github.com>
2020-08-05 01:12:21 -0300

    (#4) Setting reminder for the tests tomorrow

--- Teste Mobile/Assets/Tests/WrittenFeedbackPanelTestScript.cs
+++ Teste Mobile/Assets/Tests/WrittenFeedbackPanelTestScript.cs
@@ -12,7 +12,26 @@ namespace Tests
     [Author("Cláuvin", "")]
     public class WrittenFeedbackPanelTestScript
     {
+        [OneTimeSetUp]
+        public void WriteStartOfLog()
+        {
+            WriteTestLogScript.WriteString("WrittenFeedbackPanelTestScript tests starting.");
+        }
 
+        [SetUp]
+        public void ResetScene()
+        {
+            TestContext.WriteLine("Setup started");
+            GameObject the_main = (GameObject)GameObject.Instantiate(Resources.Load("Prefab/Prefab Main Menu"));
+            the_main.name = "Prefab Main Menu";
+            TestContext.WriteLine("Setup finished");
+        }
+
+        //Test 1: back button.
+        //Test 2: loading info.
+        //Test 3: saving info.
+        //Test 4: loading and saving info to Likert. (the other chat)
+        //Test 5: Writing name and info works.
     }
 
 }
