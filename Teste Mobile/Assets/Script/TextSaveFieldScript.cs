4c783f2435427f490d580ed9d4ab214137691d10
Cláuvin <Clauvin@users.noreply.github.com>
2020-08-04 22:33:20 -0300

    (#4) Making TextSaveFieldScript

---
+++ Teste Mobile/Assets/Script/TextSaveFieldScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSaveFieldScript : SaveFieldScript
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SaveManagerScript.save_data.ContainsKey(name))
+        {
+            SaveManagerScript.save_data.TryGetValue(name, out value);
+        }
+        else
+        {
+            value = "";
+        }
+
+        FromValueToText();
+    }
+
+    public void FromValueToText()
+    {
+        GetComponent<InputField>().text = value;
+    }
+
+    public void FromTextToValue()
+    {
+        value = GetComponent<InputField>().text;
+    }
+}
