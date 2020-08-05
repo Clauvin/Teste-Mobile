b94f0169e1549b34174393635d3a671d4389b819
Cláuvin <Clauvin@users.noreply.github.com>
2020-08-04 22:33:47 -0300

    (#4) Started WrittenFeedbackPanelTestScript

---
+++ Teste Mobile/Assets/Tests/WrittenFeedbackPanelTestScript.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    [TestFixture]
+    [Author("Cláuvin", "")]
+    public class WrittenFeedbackPanelTestScript
+    {
+
+    }
+
+}
