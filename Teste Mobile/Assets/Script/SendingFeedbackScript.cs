cd68157e6a8c80f74a6e86bb4c15b1aa90df260e
Cláuvin <Clauvin@users.noreply.github.com>
2020-08-16 16:36:39 -0300

    (#4) Changing const messsages access to public,
    for testing

--- Teste Mobile/Assets/Script/SendingFeedbackScript.cs
+++ Teste Mobile/Assets/Script/SendingFeedbackScript.cs
@@ -8,11 +8,11 @@ public class SendingFeedbackScript : MonoBehaviour
     public GameObject name_input_field;
     public GameObject considerations_input_field;
 
-    private const string good_title_message = "Thanks";
-    private const string good_body_message = "Thanks for your feedback, you can uninstall this app now. :)";
+    public const string good_title_message = "Thanks";
+    public const string good_body_message = "Thanks for your feedback, you can uninstall this app now. :)";
 
-    private const string bad_title_message = "Something went wrong";
-    private const string bad_body_message = "Something went wrong, please enter in contact with" +
+    public const string bad_title_message = "Something went wrong";
+    public const string bad_body_message = "Something went wrong, please enter in contact with" +
         " almeidaclauvin@gmail.com about it.";
 
     public void SendingFeedback()
