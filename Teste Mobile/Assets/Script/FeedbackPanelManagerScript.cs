5799d6800d5ae8ac4c6b3cdfd8ab4620c1fae6fd
Cláuvin <Clauvin@users.noreply.github.com>
2020-08-17 10:38:59 -0300

    (#5) Updated the panel manager scripts
    to reflect the new parent class.

--- Teste Mobile/Assets/Script/FeedbackPanelManagerScript.cs
+++ Teste Mobile/Assets/Script/FeedbackPanelManagerScript.cs
@@ -1,50 +1,46 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class FeedbackPanelManagerScript : MonoBehaviour
+public class FeedbackPanelManagerScript : PanelManagerScript
 {
     public GameObject likertFeedbackPanel;
     public GameObject writtenFeedbackPanel;
-    public GameObject sendFeedbackPanel;
 
     public Button backToMainButton;
     public Button nextToWrittenFeedbackButton;
-    public Button backToLikertFeedbackPanel;
-    public Button nextToSendFeedbackPanel;
+    public Button backToLikertFeedbackPanelButton;
     public Button backToWrittenFeedbackButton;
     public Button sendButton;
 
     public const string result_message_show_likert_feedback_panel_true = "showLikertFeedbackPanel true";
     public const string result_message_show_likert_feedback_panel_false = "showLikertFeedbackPanel false";
+
     public const string result_message_hide_likert_feedback_panel_true = "hideLikertFeedbackPanel true";
     public const string result_message_hide_likert_feedback_panel_false = "hideLikertFeedbackPanel false";
+
     public const string result_message_show_written_feedback_panel_true = "showWrittenFeedbackPanel true";
     public const string result_message_show_written_feedback_panel_false = "showWrittenFeedbackPanel false";
+
     public const string result_message_hide_written_feedback_panel_true = "hideWrittenFeedbackPanel true";
     public const string result_message_hide_written_feedback_panel_false = "hideWrittenFeedbackPanel false";
+
     public const string result_message_show_send_feedback_panel_true = "showSendFeedbackPanel true";
     public const string result_message_show_send_feedback_panel_false = "showSendFeedbackPanel false";
+
     public const string result_message_hide_send_feedback_panel_true = "hideSendFeedbackPanel true";
     public const string result_message_hide_send_feedback_panel_false = "hideSendFeedbackPanel false";
 
-    void Awake()
-    {
-
-    }
-
-    // Use this for initialization
     void Start()
     {
         likertFeedbackPanel.SetActive(false);
         writtenFeedbackPanel.SetActive(false);
-        sendFeedbackPanel.SetActive(false);
     }
 
     public string showLikertFeedbackPanel()
     {
         setPanel(likertFeedbackPanel, true);
 
-        return sendMessageAboutActive(likertFeedbackPanel, true, result_message_show_likert_feedback_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(likertFeedbackPanel, true, result_message_show_likert_feedback_panel_true,
             result_message_show_likert_feedback_panel_false);
     }
 
@@ -52,7 +48,7 @@ public class FeedbackPanelManagerScript : MonoBehaviour
     {
         setPanel(likertFeedbackPanel, false);
 
-        return sendMessageAboutActive(likertFeedbackPanel, false, result_message_hide_likert_feedback_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(likertFeedbackPanel, false, result_message_hide_likert_feedback_panel_true,
             result_message_hide_likert_feedback_panel_false);
     }
 
@@ -60,7 +56,7 @@ public class FeedbackPanelManagerScript : MonoBehaviour
     {
         setPanel(writtenFeedbackPanel, true);
 
-        return sendMessageAboutActive(writtenFeedbackPanel, true, result_message_show_written_feedback_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(writtenFeedbackPanel, true, result_message_show_written_feedback_panel_true,
             result_message_show_written_feedback_panel_false);
     }
 
@@ -68,24 +64,8 @@ public class FeedbackPanelManagerScript : MonoBehaviour
     {
         setPanel(writtenFeedbackPanel, false);
 
-        return sendMessageAboutActive(writtenFeedbackPanel, false, result_message_hide_written_feedback_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(writtenFeedbackPanel, false, result_message_hide_written_feedback_panel_true,
             result_message_hide_written_feedback_panel_false);
-    }
-
-    public string showSendFeedbackPanel()
-    {
-        setPanel(sendFeedbackPanel, true);
-
-        return sendMessageAboutActive(sendFeedbackPanel, true, result_message_show_send_feedback_panel_true,
-            result_message_show_send_feedback_panel_false);
-    }
-
-    public string hideMainFeedbackPanel()
-    {
-        setPanel(sendFeedbackPanel, false);
-
-        return sendMessageAboutActive(sendFeedbackPanel, false, result_message_hide_send_feedback_panel_true,
-            result_message_hide_send_feedback_panel_false);
     }
     
     private void setPanel(GameObject panel, bool true_or_false)
@@ -93,10 +73,6 @@ public class FeedbackPanelManagerScript : MonoBehaviour
         panel.SetActive(true_or_false);
     }
 
-    private string sendMessageAboutActive(GameObject oobject, bool if_equal_this, string then_text, string else_text)
-    {
-        if (oobject.activeSelf == if_equal_this) return then_text;
-        else return else_text;
-    }
+
     
 }
--- Teste Mobile/Assets/Script/MainPanelManagerScript.cs
+++ Teste Mobile/Assets/Script/MainPanelManagerScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class MainPanelManagerScript : MonoBehaviour
+public class MainPanelManagerScript : PanelManagerScript
 {
     public GameObject mainPanel;
     public GameObject guidelinesPanel;
@@ -15,27 +15,28 @@ public class MainPanelManagerScript : MonoBehaviour
 
     public const string result_message_show_main_panel_true = "showMainPanel true";
     public const string result_message_show_main_panel_false = "showMainPanel false";
+
     public const string result_message_hide_main_panel_true = "hideMainPanel true";
     public const string result_message_hide_main_panel_false = "hideMainPanel false";
+
     public const string result_message_show_guidelines_panel_true = "showGuidelinesPanel true";
     public const string result_message_show_guidelines_panel_false = "showGuidelinesPanel false";
+
     public const string result_message_hide_guidelines_panel_true = "hideGuidelinesPanel true";
     public const string result_message_hide_guidelines_panel_false = "hideGuidelinesPanel false";
+
     public const string result_message_show_feedback_panel_true = "showFeedbackPanel true";
     public const string result_message_show_feedback_panel_false = "showFeedbackPanel false";
+
     public const string result_message_hide_feedback_panel_true = "hideFeedbackPanel true";
     public const string result_message_hide_feedback_panel_false = "hideFeedbackPanel false";
+
     public const string result_message_show_about_panel_true = "showAboutPanel true";
     public const string result_message_show_about_panel_false = "showAboutPanel false";
+
     public const string result_message_hide_about_panel_true = "hideAboutPanel true";
     public const string result_message_hide_about_panel_false = "hideAboutPanel false";
 
-    void Awake()
-    {
-
-    }
-
-    // Use this for initialization
     void Start()
     {
         mainPanel.SetActive(true);
@@ -46,14 +47,14 @@ public class MainPanelManagerScript : MonoBehaviour
 
     public string loadGuidelinesCheck()
     {
-        return "todo";
+        return "not needed at the moment";
     }
 
     public string showMainPanel()
     {
         setPanel(mainPanel, true);
 
-        return sendMessageAboutActive(mainPanel, true, result_message_show_main_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(mainPanel, true, result_message_show_main_panel_true,
             result_message_show_main_panel_false);
     }
 
@@ -61,7 +62,7 @@ public class MainPanelManagerScript : MonoBehaviour
     {
         setPanel(mainPanel, false);
 
-        return sendMessageAboutActive(mainPanel, false, result_message_hide_main_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(mainPanel, false, result_message_hide_main_panel_true,
             result_message_hide_main_panel_false);
     }
 
@@ -69,7 +70,7 @@ public class MainPanelManagerScript : MonoBehaviour
     {
         setPanel(guidelinesPanel, true);
 
-        return sendMessageAboutActive(guidelinesPanel, true, result_message_show_guidelines_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(guidelinesPanel, true, result_message_show_guidelines_panel_true,
             result_message_show_guidelines_panel_false);
     }
 
@@ -77,7 +78,7 @@ public class MainPanelManagerScript : MonoBehaviour
     {
         setPanel(guidelinesPanel, false);
 
-        return sendMessageAboutActive(guidelinesPanel, false, result_message_hide_guidelines_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(guidelinesPanel, false, result_message_hide_guidelines_panel_true,
             result_message_hide_guidelines_panel_false);
     }
 
@@ -85,7 +86,7 @@ public class MainPanelManagerScript : MonoBehaviour
     {
         setPanel(feedbackPanel, true);
 
-        return sendMessageAboutActive(feedbackPanel, true, result_message_show_feedback_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(feedbackPanel, true, result_message_show_feedback_panel_true,
             result_message_show_feedback_panel_false);
     }
 
@@ -93,7 +94,7 @@ public class MainPanelManagerScript : MonoBehaviour
     {
         setPanel(feedbackPanel, false);
 
-        return sendMessageAboutActive(feedbackPanel, false, result_message_hide_feedback_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(feedbackPanel, false, result_message_hide_feedback_panel_true,
             result_message_hide_feedback_panel_false);
     }
 
@@ -101,7 +102,7 @@ public class MainPanelManagerScript : MonoBehaviour
     {
         setPanel(aboutPanel, true);
 
-        return sendMessageAboutActive(aboutPanel, true, result_message_show_about_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(aboutPanel, true, result_message_show_about_panel_true,
             result_message_show_about_panel_false);
     }
 
@@ -109,19 +110,8 @@ public class MainPanelManagerScript : MonoBehaviour
     {
         setPanel(aboutPanel, false);
 
-        return sendMessageAboutActive(aboutPanel, false, result_message_hide_about_panel_true,
+        return sendMessageAboutIfTheObjectIsActive(aboutPanel, false, result_message_hide_about_panel_true,
             result_message_hide_about_panel_false);
-    }
-
-    private void setPanel(GameObject panel, bool true_or_false)
-    {
-        panel.SetActive(true_or_false);
-    }
-
-    private string sendMessageAboutActive(GameObject oobject, bool if_equal_this, string then_text, string else_text)
-    {
-        if (oobject.activeSelf == if_equal_this) return then_text;
-        else return else_text;
     }
 
     public void exitProgram()
