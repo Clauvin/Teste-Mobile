1a9e9de81d7f6e8ccf0ea86903c8c02ecefc4da9
Cláuvin <Clauvin@users.noreply.github.com>
2020-08-17 10:37:08 -0300

    (#5) Created PanelManagerScript

---
+++ Teste Mobile/Assets/Script/PanelManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManagerScript : MonoBehaviour
+{
+    protected void setPanel(GameObject panel, bool true_or_false)
+    {
+        panel.SetActive(true_or_false);
+    }
+
+    protected string sendMessageAboutIfTheObjectIsActive(GameObject the_object, bool if_equal_this,
+        string then_text, string else_text)
+    {
+        if (the_object.activeSelf == if_equal_this) return then_text;
+        else return else_text;
+    }
+
+
+}
